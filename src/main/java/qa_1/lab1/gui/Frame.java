455b7fb4fdb8c3294410d20664a286f2505bb7a7
nikolaynest <nnicko850@gmail.com>
2014-02-14 15:04:01 +0200

    Lab1 works. That's enough..

--- src/main/java/qa_1/lab1/gui/Frame.java
+++ src/main/java/qa_1/lab1/gui/Frame.java
@@ -8,8 +8,6 @@ import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.table.AbstractTableModel;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -25,30 +23,15 @@ public class Frame extends JFrame {
     private Fridge fridge;
     private JToggleButton button;
     private JLabel label;
-    JLabel hasFood;
+    JLabel hasFoodLabel;
     final MyTableModel myTableModel = new MyTableModel();
 
-    public void some(){
-
-    }
-
-    public Frame() {
-        super("ХОЛОДИЛЬНИК");
-        initFridge();
-
-        ImageIcon green = new ImageIcon("images/green.jpg");
-        ImageIcon red = new ImageIcon("images/red.jpg");
-
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(700, 300);
-
-        JPanel panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-
+    public JPanel firstPanel(){
         JPanel header = new JPanel();
-        label = new JLabel("Выключен");
+        label = new JLabel("Включен");
         button = new JToggleButton("ON/OFF");
-        button.setSize(200, 500);
+        button.setSelected(true);
+//        button.setSize(200, 400);
         button.addItemListener(new ItemListener() {
             @Override
             public void itemStateChanged(ItemEvent e) {
@@ -63,22 +46,26 @@ public class Frame extends JFrame {
         });
         header.add(button);
         header.add(label);
+        return header;
+    }
 
+    public JPanel secondPanel(){
         JPanel infoPanel = new JPanel();
-        hasFood = new JLabel("Холодильник пуст");
-        JButton update = new JButton("Проверить продукты");
-        update.addActionListener(new ActionListener() {
+        hasFoodLabel = new JLabel("Холодильник пуст");
+        JButton updateButton = new JButton("Проверить продукты");
+        updateButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 myTableModel.fireTableDataChanged();
             }
         });
-        infoPanel.add(hasFood);
-        infoPanel.add(update);
+        infoPanel.add(hasFoodLabel);
+        infoPanel.add(updateButton);
+        return infoPanel;
+    }
 
-
+    public JPanel thirdPanel(){
         JPanel foodPanel = new JPanel();
-
         final JButton delete = new JButton("Вынуть из холодильника");
         delete.setEnabled(false);
         final JTable listTable = new JTable(myTableModel);
@@ -111,32 +98,49 @@ public class Frame extends JFrame {
         JScrollPane jScrollPane = new JScrollPane(listTable);
         listTable.setPreferredScrollableViewportSize(new Dimension(250, 100));
 
-        JLabel jlb = new JLabel("Введите название продукта");
+        JLabel jlb = new JLabel("Введите название продукта:");
         final JTextField foodName = new JTextField(18);
+
         JButton foodButton = new JButton("Положить в холодильник");
         foodButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 fridge.putFoodIn(new Food(foodName.getText(),
                         Calendar.getInstance().get(Calendar.DAY_OF_YEAR)));
+                foodName.setText("");
                 if (!fridge.isEmpty()) {
-                    hasFood.setText("В холодильнике есть продукты");
+                    hasFoodLabel.setText("В холодильнике есть продукты");
                 } else {
-                    hasFood.setText("Холодильник пуст");
+                    hasFoodLabel.setText("Холодильник пуст");
                 }
                 myTableModel.fireTableDataChanged();
             }
         });
-        foodPanel.add(delete);
-        foodPanel.add(jlb);
-        foodPanel.add(foodName);
-        foodPanel.add(foodButton);
-        foodPanel.add(jScrollPane);
+        Box box = Box.createVerticalBox();
+        box.setAlignmentX(CENTER_ALIGNMENT);
+        box.add(jlb);
+        box.add(Box.createVerticalStrut(5));
+        box.add(foodName);
+        box.add(Box.createVerticalStrut(5));
+        box.add(foodButton);
+        box.add(Box.createVerticalStrut(20));
+        box.add(jScrollPane);
+        box.add(Box.createVerticalStrut(10));
+        box.add(delete);
+        foodPanel.add(box);
+        return foodPanel;
+    }
 
-        panel.add(header);
-        panel.add(infoPanel);
-        panel.add(foodPanel);
-
+    public Frame() {
+        super("ХОЛОДИЛЬНИК");
+        initFridge();
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(400, 500);
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.add(firstPanel());
+        panel.add(secondPanel());
+        panel.add(thirdPanel());
         add(panel);
         setVisible(true);
     }
